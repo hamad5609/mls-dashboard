84c672f66eb8013b8002a4effbec12c581427523
hamadhameed <hheview@gmail.com>
2021-11-05 21:45:27 +0500

    create procfile

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const PostsRouter = require('./Routes/posts');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+var PORT = process.env.PORT || 8000;
 dotenv.config();
 app.use(cors());
 app.use(express.json());
@@ -18,4 +19,4 @@ app.use('/uploads', express.static('uploads'));
 mongoose.connect(process.env.DATA_BASE_AUTH, () => console.log('Database created'))
 
 
-app.listen(8000)
+app.listen(PORT)
